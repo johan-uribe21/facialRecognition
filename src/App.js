37f88b3e590920674d68938f9eda6e9892ef9fd0
Johan Uribe <Johanandresuribe@gmail.com>
2019-04-05 00:15:43 -0400

    minor bug fixes

--- src/App.js
+++ src/App.js
@@ -94,7 +94,7 @@ class App extends Component {
     let input = this.state.input
     this.setState({imageURL: input});
     //console.log('submit', this.state.imageURL ) ;
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://vast-wildwood-78447.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -104,7 +104,7 @@ class App extends Component {
       .then(response => response.json())
       .then( response => { 
         if (response){
-          fetch('http://localhost:3000/image', {
+          fetch('https://vast-wildwood-78447.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
